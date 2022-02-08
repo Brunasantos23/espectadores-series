a32cef893172070c56c9a72ddb11a496068c46ea
Bruna <brunasantos@dcomp.ufs.br>
2022-02-08 08:25:45 -0300

    atualização do banco

--- database/seeders/Servico_streming_has_espectadorSeeder.php
+++ database/seeders/Servico_streming_has_espectadorSeeder.php
@@ -17,8 +17,15 @@ class Servico_streming_has_espectadorSeeder extends Seeder
     public function run()
     {
         DB::table('espectador_has_servico_streaming')->insert([
-            'servico_streaming_id' => 9,
-            'espectador_id' => 4,
+            'servico_streaming_id' => 1,
+            'espectador_id' => 1,
+            'created_at' =>'2022-01-28 9',
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('espectador_has_servico_streaming')->insert([
+            'servico_streaming_id' => 2,
+            'espectador_id' => 2,
             'created_at' =>'2022-01-28 9',
             'updated_at' => Carbon::now(),
         ]);
