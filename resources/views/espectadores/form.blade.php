c13d49d2dd6bd3beba08e8d2cc5cdfd0bc3317af
Bruna <brunasantos@dcomp.ufs.br>
2022-02-10 13:35:19 -0300

    views formulário

---
+++ resources/views/espectadores/form.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" lang="pt-br"/>
+    </head>
+    <body>
+
+         <form  method="POST" action="#" name="form">
+
+             <div>
+
+                 <label for="nome">Nome:</label>
+                 <input type="text" id="nome"><br/>
+
+                 <label for="idade">Idade:</label>
+                 <input type="int" id="idade">
+
+             </div>
+
+         </form>
+
+    </body>
+</html>
