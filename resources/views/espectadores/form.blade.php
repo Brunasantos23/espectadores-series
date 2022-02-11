957daf1c73941a4efc8b1eed9e221fc026045172
Bruna <brunasantos@dcomp.ufs.br>
2022-02-11 10:40:32 -0300

    views com bootstrap

--- resources/views/espectadores/form.blade.php
+++ resources/views/espectadores/form.blade.php
@@ -1,41 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" lang="pt-br"/>
-    </head>
-    <body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        @if(isset($espectador))
-           <form method="POST" action="{{route('espectador.update',$espectador->id)}}" accept-charset="UTF-8" name="form">
-            @method('PUT')
-        @else
-           <form  method="POST" action="{{ route('espectador.store') }}" name="form">
-        @endif
-            @csrf
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-             <div>
+    <title>Criação de espectador</title>
+  </head>
+  <body>
+    <h1>Criação de espectador</h1>
 
-                 <label for="nome">Nome:</label>
-                 <input type="text"  value="{{$espectador->nome??''}}" id="nome" name="nome"><br/>
+    @if(isset($espectador))
+    {!! Form::open(['route' => array('espectador.update'), 'method' => 'PUT', 'name' => 'form'])!!}
+    @else
+    {!! Form::open(['route' => array('espectador.store'), 'method' => 'POST', 'name' => 'form'])!!}
+    @endif
 
-                 <label for="idade">Idade:</label>
-                 <input type="int" value="{{$espectador->idade??''}}" id="idade" name="idade"><br/>
+    {!!Form::label('nome', 'Nome:', ['class' => 'form-check-label'])!!}
+    {!!Form::text('nome',   isset($espectador) ? $espectador->nome : null, ['class' => 'form-control','placeholder' => 'Somente Letras',  $form??null])!!}
 
-                 <input type="submit" value="Enviar">
+    {!! Form::close() !!}
 
-             </div>
-
-         </form>
-
-         @if(isset($espectador))
-            <form  method="POST" action="{{ route('espectador.destroy',$espectador->id) }}" name="form">
-                @method('DELETE')
-                @csrf
-
-              <input type="submit" value="Deletar">
-            </form>
-         @endif
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
-    </body>
+  </body>
 </html>
